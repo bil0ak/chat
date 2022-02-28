3e8a9f8b3ea5dedf4137e7c4338a0fd6ea44e7d1
Bilal Akkil <bilal0akkil@gmail.com>
2022-03-01 00:53:05 +0400

    .

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// modified from the sockit.io example.
+
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+var id = "room";
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
-  });
+app.get("/chat/:id", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+  id = req.params.id;
 });
+
+io.on("connection", async (socket) => {
+  const userId = socket.id;
+
+  socket.join(id);
+
+  console.log(socket.rooms);
+
+  socket.on("chat message", (msg, usID) => {
+    io.to(id).emit("chat message", msg, usID);
+  });
+
+  io.to(id).emit("hi");
+
+});
+
 
 http.listen(port, () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
