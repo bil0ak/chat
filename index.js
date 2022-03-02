8c885a9837da1d937a7195ccbb97f5faf3503b20
Bilal Akkil <bilal0akkil@gmail.com>
2022-03-02 14:25:05 +0400

    auto create new chat with uuid

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
+const { v4: uuidv4 } = require('uuid');
 const port = process.env.PORT || 3000;
 
 var id = "room";
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.redirect('/chat/'+uuidv4())
+  // res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/chat/:id", function (req, res) {
